de68ca89d2196cda17030303eaf137fb105cbbe7
youpy <youpy@buycheapviagraonlinenow.com>
2009-04-06 07:55:35 +0900

    * add Scissor::Sequence, Scissor.sequence  * update rdoc  * version0.0.14

--- lib/scissor.rb
+++ lib/scissor.rb
@@ -267,6 +267,10 @@ class Scissor
         slice(0, 1).
         fill(duration)
     end
+
+    def sequence(*args)
+      Sequence.new(*args)
+    end
   end
 
   class SoundFile
@@ -312,6 +316,34 @@ class Scissor
 
     def reversed?
       @reverse
+    end
+  end
+
+  class Sequence
+    def initialize(pattern, duration_per_step)
+      @pattern = pattern
+      @duration_per_step = duration_per_step
+    end
+
+    def apply(scissors)
+      result = Scissor.new
+
+      @pattern.split(//).each do |c|
+        if scissors.include?(c.to_sym)
+          scissor = scissors[c.to_sym]
+
+          if @duration_per_step > scissor.duration
+            result += scissor
+            result += Scissor.silence(@duration_per_step - scissor.duration)
+          else
+            result += scissors[c.to_sym].slice(0, @duration_per_step)
+          end
+        else
+          result += Scissor.silence(@duration_per_step)
+        end
+      end
+
+      result
     end
   end
 end
---
+++ spec/sequence_spec.rb
@@ -0,0 +1,49 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'spec_helper'
+
+describe Scissor::Sequence do
+  before do
+    @foo = Scissor.new(fixture('sample.mp3'))
+    @bar = Scissor.new(fixture('sine.wav'))
+  end
+
+  it "should instantiated" do
+    seq = Scissor.sequence('ababaab', 1.5)
+    seq.should be_an_instance_of(Scissor::Sequence)
+  end
+
+  it "should apply" do
+    seq = Scissor.sequence('ababaab ab', 0.5)
+    scissor = seq.apply(:a => @foo, :b => @bar)
+
+    scissor.should be_an_instance_of(Scissor)
+    scissor.duration.should eql(5.0)
+    scissor.fragments.size.should eql(10)
+
+    scissor.fragments.each do |fragment|
+      fragment.duration.should eql(0.5)
+    end
+
+    scissor.fragments[0].filename.to_s.should eql(fixture('sample.mp3'))
+    scissor.fragments[1].filename.to_s.should eql(fixture('sine.wav'))
+    scissor.fragments[2].filename.to_s.should eql(fixture('sample.mp3'))
+    scissor.fragments[3].filename.to_s.should eql(fixture('sine.wav'))
+    scissor.fragments[4].filename.to_s.should eql(fixture('sample.mp3'))
+    scissor.fragments[5].filename.to_s.should eql(fixture('sample.mp3'))
+    scissor.fragments[6].filename.to_s.should eql(fixture('sine.wav'))
+    scissor.fragments[8].filename.to_s.should eql(fixture('sample.mp3'))
+    scissor.fragments[9].filename.to_s.should eql(fixture('sine.wav'))
+  end
+
+  it "should append silence when applied instance does not have enough duration" do
+    seq = Scissor.sequence('ba', 1.5)
+    scissor = seq.apply(:a => @foo, :b => @bar)
+
+    scissor.duration.should eql(3.0)
+    scissor.fragments.size.should eql(3)
+    scissor.fragments[0].filename.to_s.should eql(fixture('sine.wav'))
+    scissor.fragments[1].filename.to_s.should match(/silence\.mp3/)
+    scissor.fragments[2].filename.to_s.should eql(fixture('sample.mp3'))
+  end
+end
