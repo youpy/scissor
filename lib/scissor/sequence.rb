f989ee6f76ef1d89c4246ca3a8ba6e24a2857ffb
youpy <youpy@buycheapviagraonlinenow.com>
2009-07-19 11:30:16 +0900

    refactored Scissor::Sequence#apply

--- lib/scissor/sequence.rb
+++ lib/scissor/sequence.rb
@@ -6,9 +6,7 @@ module Scissor
     end
 
     def apply(instruments)
-      result = Scissor()
-
-      @pattern.split(//).each do |c|
+      @pattern.split(//).inject(Scissor()) do |result, c|
         if instruments.include?(c.to_sym)
           instrument = instruments[c.to_sym]
 
@@ -17,17 +15,16 @@ module Scissor
           end
 
           if @duration_per_step > instrument.duration
-            result += instrument
-            result += Scissor.silence(@duration_per_step - instrument.duration)
+            result += instrument + Scissor.silence(@duration_per_step - instrument.duration)
           else
             result += instrument.slice(0, @duration_per_step)
           end
         else
           result += Scissor.silence(@duration_per_step)
         end
-      end
 
-      result
+        result
+      end
     end
   end
 end
